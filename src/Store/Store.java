77c2a330b18bb01bb6e69c18dd745b7bfbbb4483
akharchenko23 <akharchenko@kse.org.ua>
2024-06-09 21:36:48 +0300

    конфлікти комітів зводять мене з розуму

--- src/Store/Store.java
+++ src/Store/Store.java
@@ -25,7 +25,7 @@ public class Store {
     public MoneyDisplay moneyDisplay = new MoneyDisplay(5, 20, 50, 50);
     public Chair chair = new Chair(100, 120, 200, 200, 500);
     public Table table = new Table(400, 120, 200, 200, 1000);
-    public GoldenSpatula goldenSpatula = new GoldenSpatula(750, 120, 90, 200, 1500);
+    public GoldenSpatula goldenSpatula = new GoldenSpatula(700, 120, 200, 200, 1500);
     public Music music = new Music(100, 400, 200, 200, 2000);
     public Painting painting = new Painting(400, 400, 200, 200, 2500);
     public IconK iconK = new IconK(700, 400, 200, 200, 3000);
@@ -59,29 +59,27 @@ public class Store {
     }
 
     public void drawStore(Graphics2D g2d) {
-        busketButton.draw(g2d);
-        openBusket();
+        //busketButton.draw(g2d);
+       // openBusket();
         drawAllAccessories(g2d);
         buyAccessories();
     }
 
-    //цю какаху треба переробити
+
     private void drawAllAccessories(Graphics2D g2d) {
-        int startX = 100;
-        int startY = 120;
-        int xOffset = 300;
-        int yOffset = 280;
-        int col = 0; //3 стовпці
-        //що це??? навіщо це??? з якою метою??? воно вбиває гру??? навіщо???
-        //якщо це кнопки для покупки то зроби це якось інакше бо в мене пк не витримує таких приколів
-        for (int row = 0; row < 2; row++) { // 2 рядки
-//            ProductBack productBack = new ProductBack(startX + col * xOffset, startY + row * yOffset, 200, 260);
-//            productBack.draw(g2d);
-//            col++;
-//            if (col == 2)
-//                col = 0;
-        }
+
         drawAccessory(g2d);
+        drawPrices(g2d);
+    }
+
+    private void drawPrices(Graphics2D g2d) {
+        g2d.drawString(" Price: $" + 500, 100, 100);
+        g2d.drawString(" Price: $" + 1000, 400, 100);
+        g2d.drawString(" Price: $" + 1500, 700, 100);
+        g2d.drawString(" Price: $" + 2000, 100, 380);
+        g2d.drawString(" Price: $" + 2500, 400, 380);
+        g2d.drawString(" Price: $" + 3000, 700, 380);
+
     }
 
     private void drawAccessory(Graphics2D g2d){
@@ -97,44 +95,41 @@ public class Store {
 
     //я трохи почистила цей метод
     private void buyAccessories() {
-        if (!Game.mouse.pressed && paid) {
-            paid = false;
-            return;
-        }
-        for (Accessories accessory : accessories) {
-            if (!(!accessory.getFileName().equals("/store/lockedproduct.png") && Game.mouse.pressed && Game.mouse.x >= accessory.getX() && Game.mouse.x <= accessory.getX() + 200 && Game.mouse.y <= accessory.getY() + 200 && Game.mouse.y >= accessory.getY() && parent.money >= accessory.getPrice()))
-                continue;
-            switch (accessory.getFileName()) {
-                case "/store/goldenspatula.png" -> {
-                    System.out.println(accessory.getFileName() + " " + accessory.getPrice());
-                    goldenSpatulaBought = true;
-                }
-                case "/store/music.png" -> {
-                    System.out.println(accessory.getFileName() + " " + accessory.getPrice());
-                    musicBought = true;
-                }
-                case "/store/picture.png" -> {
-                    System.out.println(accessory.getFileName() + " " + accessory.getPrice());
-                    paintingBought = true;
-                }
-                case "/store/icon.png" -> {
-                    System.out.println(accessory.getFileName() + " " + accessory.getPrice());
-                    iconBought = true;
-                }
-                case "/store/chair.png" -> {
-                    System.out.println(accessory.getFileName() + " " + accessory.getPrice());
-                    chairBought = true;
-                }
-                case "/store/table.png" -> {
-                    System.out.println(accessory.getFileName() + " " + accessory.getPrice());
-                    tableBought = true;
+        if (Game.mouse.pressed && !paid) {
+            for (Accessories accessory : accessories) {
+                if (!accessory.getFileName().equals("/store/lockedproduct.png") && Game.mouse.pressed && Game.mouse.x >= accessory.getX() && Game.mouse.x <= accessory.getX() + 200 && Game.mouse.y <= accessory.getY() + 200 && Game.mouse.y >= accessory.getY() && parent.money >= accessory.getPrice()) {
+                    if (accessory.getFileName().equals("/store/goldenspatula.png")) {
+                        System.out.println(accessory.getFileName() + " " + accessory.getPrice());
+                        goldenSpatulaBought = true;
+                    } else if (accessory.getFileName().equals("/store/music.png")) {
+                        System.out.println(accessory.getFileName() + " " + accessory.getPrice());
+                        musicBought = true;
+                    } else if (accessory.getFileName().equals("/store/picture.png")) {
+                        System.out.println(accessory.getFileName() + " " + accessory.getPrice());
+                        paintingBought = true;
+                    } else if (accessory.getFileName().equals("/store/icon.png")) {
+                        System.out.println(accessory.getFileName() + " " + accessory.getPrice());
+                        iconBought = true;
+                    } else if (accessory.getFileName().equals("/store/chair.png")) {
+                        System.out.println(accessory.getFileName() + " " + accessory.getPrice());
+                        chairBought = true;
+                    } else if (accessory.getFileName().equals("/store/table.png")) {
+                        System.out.println(accessory.getFileName() + " " + accessory.getPrice());
+                        tableBought = true;
+                    }
+                    accessory.startFalling();
+                    boughtAccessoriesArrayList.add(accessory);
+                    parent.money -= accessory.getPrice();
+                    moneyDisplay.setCurrentMoney(parent.money);
+                    break;
                 }
             }
-//            accessory.startFalling();
-            boughtAccessoriesArrayList.add(accessory);
-            parent.money -= accessory.getPrice();
+            paid = true;
         }
-        paid = true;
+
+        if (!Game.mouse.pressed) {
+            paid = false;
+        }
     }
 
     public void openBusket() {
